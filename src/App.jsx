4faac0b8fc0ac6fe781e1e6dff80c7f425298058
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-06-06 22:59:17 -0300

    Adição do código

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function App() {
+  return (
+
+    <div className="flex w-full h-screen items-center justify-center">
+
+      <form>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold"
+        >
+          Pesquisar
+        </button>
+      </form>
+
+    </div>
+
+  );
+}
+
+export default App;
