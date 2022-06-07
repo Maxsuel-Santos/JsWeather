eaee132866c800c41cdfa4f9d91ec3999c1495b0
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-06-07 14:26:58 -0300

    Add Code React

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fetchData from './services/api';
 import Card from './components/Card/Card';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [data, setData] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetchData(city).then((response) => {
+      setData(response);
+    });
+  };
+
   return (
 
     <div className="flex flex-col w-full h-screen items-center justify-center">
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Cidade"
           className="p-3 rounded-lg outline-none"
+          value={city}
+          onChange={({ target: { value } }) => setCity(value)}
         />
         <button
           type="submit"
@@ -20,7 +34,7 @@ function App() {
         </button>
       </form>
 
-      <Card />
+      <Card data={data} />
 
     </div>
 
--- src/services/api.js
+++ src/services/api.js
@@ -7,8 +7,9 @@ const fetchData = async (city) => {
   const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
 
-  console.log(fetchResponse);
+  return data;
 };
 
 export default fetchData;
